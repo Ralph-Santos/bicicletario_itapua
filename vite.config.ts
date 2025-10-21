fe195515fde232bbd90680c83d030a929502b082
Raplh-Santos <rb20102001@gmail.com>
2025-10-21 19:27:37 -0300

    fix - falha ao carregar main

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+base: mode === "development" ? "/" : "/itapua-cycle-hub/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
